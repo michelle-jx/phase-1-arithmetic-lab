6afd7b09483399172642bcdaea8cb13bdae70e93
Michelle Too <mjxtcodes@gmail.com>
2022-09-20 09:25:20 -0700

    commit tree 3bfa0ac5be0e4fd5c538938d90c26554aa9804e5

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = (num1 * num2)
+
+function random() {
+    return (Math.floor(math.random() * 10) + 1)
+}
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+
+numberSet = [1, 2, 3, 4, 20]
+function max(numberSet) {
+    return Math.max(numberSet)
+}
